497258735709ba3feef87a73fd40cb42aa4770da
jermu01 <49148862+jermu01@users.noreply.github.com>
2021-09-17 09:43:45 +0300

    .

--- website/gallery.php
+++ website/gallery.php
@@ -21,21 +21,6 @@ body {
 
 <style>
 
-.zoom {
-  padding: 100px;
-  transition: transform .2s;
-  width: 50px;
-  height: 50px;
-  margin: 0 auto;
-}
-
-.zoom:hover {
-  -ms-transform: scale(1.5); /* IE 9 */
-  -webkit-transform: scale(1.5); /* Safari 3-8 */
-  transform: scale(1.5); 
-}
-
-
 
 h1 {
   text-align: center;
@@ -46,13 +31,11 @@ h1 {
 <div class="container">
   <div class="jumbotron" style="text-align: center;">
     <h1>Kuvia</h1>
-</center><legend> Kuvat zoomautuu</legend></center>
     
       <div class="container">
         <div class="row">
         <div class="card text-center">
         <div id="posts-container">
-        <div class="zoom">
         <br><br>
         </div>
       </div>
