3a4829568721fcf6d4bb711649da6a55657f1072
jermu01 <49148862+jermu01@users.noreply.github.com>
2021-10-06 14:36:39 +0300

    .

--- backend/db.php
+++ backend/db.php
@@ -2,15 +2,21 @@
 
 //database information
 $servername = "localhost";
-$username = "pkpirtti_user";
-$password = "foFIKEW{Y-}8";
+$db_username = "pkpirtti_user";
+$db_password = "foFIKEW{Y-}8";
 $database = "pkpirtti_pirtti_db";
+
+//testing
+// $servername = "localhost";
+// $db_username = "root";
+// $db_password = "";
+// $database = "pirtti";
 
 $url = "https://localhost/pirtti/backend/"; //mail url
 
 //database connection
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $db_username, $db_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
